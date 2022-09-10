49de753e7d5fa7d6a8a9e2207c2a7a08e8b8e0a9
newell212 <michaelnewell@michaels-air.lan>
2022-09-10 13:08:20 -0600

    completed tasks 3-6

--- index.js
+++ index.js
@@ -113,7 +113,14 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor({name, age, location}) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
 }
 
 /*
@@ -131,8 +138,22 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor({name, age, location, specialty, favLanguage, catchPhrase}) {
+    super({name, age, location, specialty, favLanguage, catchPhrase})
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`
+  }
 }
 
 /*
@@ -151,8 +172,27 @@ class Instructor {
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
 
-class Student {
-   
+class Student extends Lambdasian {
+   constructor({name, age, location, previousBackground, className, favSubjects}) {
+    super(name, age, location);
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubjects = favSubjects;
+   }
+   listSubjects() {
+    return `${this.favSubjects}`
+   }
+   PRAssignment(subject) {
+    this.subject = subject;
+    return `${this.name} has submitted a PR for ${this.subject}`
+   }
+   sprintChallenge(subject) {
+    this.subject = subject;
+    return `${this.name} has begun sprint challenge on ${this.subject}`
+   }
 }
 
 /*
@@ -169,8 +209,26 @@ class Student {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
+class ProjectManager extends Instructor {
+   constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}) {
+    super({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor});
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+   }
+   standUp(channel) {
+    this.channel = channel;
+    return `${this.name} announces to ${this.channel}, @channel standy times!`
+   }
+   debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+
+   }
 }
 
 /*
